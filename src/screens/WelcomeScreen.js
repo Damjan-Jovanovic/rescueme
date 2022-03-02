ac05c5b5c1e7604fd6c4fb5a76cce8de38dc6457
Damjan115 <damjan.djoko@gmail.com>
2022-03-02 11:58:00 +0100

    Page accueil

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, Text, Button, Image, View, StyleSheet, ImageBackground } from 'react-native';
+import { SafeAreaView, Text, Button, Image, View, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
 
 
 function WelcomeScreen(props) {
@@ -10,17 +10,14 @@ function WelcomeScreen(props) {
                 source={require('../assets/logoSeeThrough.png')}
             ></Image>
 
-            <View style={stylesButton.btn}>
-                <Button 
-                    title='Connexion'
-                    //onPress={() => navigate('LoginScreen')}
-                    //color etc
-                />
-                <Button 
-                    title='Inscription'
-                
-                />
-            </View>
+            <TouchableOpacity  style={styleLoginButton.loginButton}>
+                <View style={styleLoginButton.loginButton}><Text style={styleBtn.textButton}>Connexion</Text></View> 
+            </TouchableOpacity>
+            
+            <TouchableOpacity  style={styleLoginButton.registerButton}>
+                <View style={styleLoginButton.registerButton}><Text  style={styleBtn.textButton}>Inscription</Text></View>
+            </TouchableOpacity>
+
         </SafeAreaView>
     );
 }
@@ -29,7 +26,7 @@ const stylesBackground = StyleSheet.create({
     background:{
         flex: 1,
         backgroundColor: '#87CEEB',
-        justifyContent: 'center',
+        justifyContent: 'flex-end',
         alignItems: 'center',
     }
 })
@@ -38,13 +35,33 @@ const stylesMiddle = StyleSheet.create({
     middle:{
         width: 300,
         height: 300,
+        position: 'absolute',
+        top: 50
     }
 })
 
-const stylesButton = StyleSheet.create({
-    btn:{
-        
+const styleLoginButton = StyleSheet.create({
+    loginButton:{
+        width:'100%',
+        height: 100,
+        backgroundColor: '#fc5c65',
+        justifyContent: 'center'
+    },
+    registerButton:{
+        width: '100%',
+        height: 100,
+        backgroundColor: "#4ecdc4",
+        justifyContent: 'center'
     }
 })
+
+const styleBtn = StyleSheet.create({
+    textButton:{
+        fontSize: 30,
+        textAlign: 'center',
+
+    }
+})
+
 
 export default WelcomeScreen;
