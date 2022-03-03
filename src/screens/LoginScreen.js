5dff5cd238f41c108fa4f6c56c1d94b8f3742eb9
Mickaelfbr <82059684+mickaelfbr@users.noreply.github.com>
2022-03-03 16:29:15 +0100

    Installation librairie NativeBase + login

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,18 +1,62 @@
 import React from 'react';
-import { SafeAreaView, Text, Button, Image, View, StyleSheet, ImageBackground } from 'react-native';
+import { SafeAreaView, Image, View, StyleSheet, ImageBackground } from 'react-native';
 import colors from '../config/colors';
+import { NativeBaseProvider, Center, Heading, VStack, FormControl, Input, Link, Button, HStack, Text, Box } from 'native-base';
 
 const LoginScreen=({props}) =>{
     return (
-        <SafeAreaView style={stylesBackground.background}>
-            <Image
-                style={stylesHeader.image}
-                source={require('../assets/logoSeeThrough.png')}
-            ></Image>
+        <NativeBaseProvider style={stylesBackground.background}>
+            <Center w="100%">
+            <Box safeArea p="2" py="8" w="90%" maxW="290">
+                <Heading size="lg" fontWeight="600" color="coolGray.800" _dark={{
+                color: "warmGray.50"
+            }}>
+                Welcome
+                </Heading>
+                <Heading mt="1" _dark={{
+                color: "warmGray.200"
+            }} color="coolGray.600" fontWeight="medium" size="xs">
+                Sign in to continue!
+                </Heading>
 
-        </SafeAreaView>
-    );
-}
+                <VStack space={3} mt="5">
+                <FormControl>
+                    <FormControl.Label>Email ID</FormControl.Label>
+                    <Input />
+                </FormControl>
+                <FormControl>
+                    <FormControl.Label>Password</FormControl.Label>
+                    <Input type="password" />
+                    <Link _text={{
+                    fontSize: "xs",
+                    fontWeight: "500",
+                    color: "indigo.500"
+                }} alignSelf="flex-end" mt="1">
+                    Forget Password?
+                    </Link>
+                </FormControl>
+                <Button mt="2" colorScheme="indigo">
+                    Sign in
+                </Button>
+                <HStack mt="6" justifyContent="center">
+                    <Text fontSize="sm" color="coolGray.600" _dark={{
+                    color: "warmGray.200"
+                }}>
+                    I'm a new user.{" "}
+                    </Text>
+                    <Link _text={{
+                    color: "indigo.500",
+                    fontWeight: "medium",
+                    fontSize: "sm"
+                }} href="#">
+                    Sign Up
+                    </Link>
+                </HStack>
+                </VStack>
+            </Box>
+            </Center>
+        </NativeBaseProvider>);
+};
 
 const stylesBackground = StyleSheet.create({
     background:{
@@ -25,6 +69,7 @@ const stylesHeader = StyleSheet.create({
         width: 100,
         height: 100,
         position: 'absolute',
+
         top: 10
     }
 })
