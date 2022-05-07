3f8fb816012762820dcf54bbd7ffac9a7e8e88e0
Mickaelfbr <82059684+mickaelfbr@users.noreply.github.com>
2022-05-07 16:02:27 +0200

    Version stable homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,61 +1,65 @@
 import React from 'react';
+import {NativeBaseProvider,Text,Center} from "native-base";
+import {Image, StyleSheet} from "react-native";
+import SwipeButton from 'rn-swipe-button';
+import colors from '../config/colors';
  
 // import all the components we are going to use
-import {
-  SafeAreaView,
-  View,
-  Text,
-  StyleSheet
-} from 'react-native';
- 
-import SwipeButton from 'rn-swipe-button';
  
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <Text style={styles.titleStyle}>
-          Example of React Native Swipe Button
-        </Text>
-        <SwipeButton
-          disabled={false}
-          //disable the button by doing true (Optional)
-          swipeSuccessThreshold={70}
-          height={45}
-          //height of the button (Optional)
-          width={330}
-          //width of the button (Optional)
-          title="Swipe to Submit"
-          //Text inside the button (Optional)
-          //thumbIconImageSource={thumbIcon}
-          //You can also set your own icon (Optional)
-          onSwipeSuccess={() => {
-            alert('Submitted Successfully!');
-          }}
-          //After the completion of swipe (Optional)
-          railFillBackgroundColor="#e688a1" //(Optional)
-          railFillBorderColor="#e688ff" //(Optional)
-          thumbIconBackgroundColor="#ed9a73" //(Optional)
-          thumbIconBorderColor="#ed9aff" //(Optional)
-          railBackgroundColor="#bbeaa6" //(Optional)
-          railBorderColor="#bbeaff" //(Optional)
-        />
-      </View>
-    </SafeAreaView>
+    <NativeBaseProvider>
+      <Center>
+        <Image 
+            style={styles.tinyLogo}
+            source={require("../assets/profil.png")}
+          >
+          </Image>
+          <Text>
+            User name ...
+          </Text>
+          <Text>
+            If premium or junior = display info
+          </Text>
+      </Center>
+
+      {/* Swipe button for Alert */}
+      <Center>
+        
+          <SwipeButton
+          style={styles.swipebtn}
+            disabled={false}
+            //disable the button by doing true (Optional)
+            swipeSuccessThreshold={70}
+            height={150}
+            //height of the button (Optional)
+            width={330}
+            //width of the button (Optional)
+            title="Alarme !"
+            //Text inside the button (Optional)
+            //thumbIconImageSource={thumbIcon}
+            //You can also set your own icon (Optional)
+            onSwipeSuccess={() => {
+              alert('Alarm activated!');
+            }}
+            //After the completion of swipe (Optional)
+            railFillBackgroundColor="#0066CC" //(Optional)
+            //railFillBorderColor="#007FFF" //(Optional)
+            thumbIconBackgroundColor="#007FFF" //(Optional)
+            thumbIconBorderColor="#0066CC" //(Optional)
+            railBackgroundColor="#FFFFFF" //(Optional)
+            railBorderColor="#007FFF" //(Optional)
+          />
+      
+      </Center>
+    </NativeBaseProvider>
   );
 };
 export default App;
  
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    padding: 10,
-  },
-  titleStyle: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    padding: 10,
+  tinyLogo: {
+    width: 100,
+    height: 100,
   },
 });
