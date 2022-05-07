b30319973b77ee051d122666e4a43dbd4d99dd42
OmarZaghdoudi <omar.zaghdoudi@etu.hesge.ch>
2022-05-07 22:57:45 +0200

    InviteContactScreen
    Afficher la liste des contacts sans style

--- src/screens/InviteContactScreen.js
+++ src/screens/InviteContactScreen.js
@@ -1,27 +1,52 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useEffect, useState} from 'react';
 import * as Contacts from 'expo-contacts';
+import { StyleSheet, View, Text, FlatList,TouchableOpacity } from 'react-native'
 
 export default function App() {
+  const [contacts, setContacts ] = useState([])
   useEffect(() => {
     (async () => {
       const { status } = await Contacts.requestPermissionsAsync();
       if (status === 'granted') {
         const { data } = await Contacts.getContactsAsync({
-          fields: [Contacts.Fields.Emails],
+          fields: [Contacts.Fields.PhoneNumbers],
         });
 
         if (data.length > 0) {
-          const contact = data[0];
-          console.log(contact);
+          setContacts(data)
         }
       }
     })();
   }, []);
 
   return (
-    <View>
-      <Text>Contacts Module Example</Text>
+    <View style={styles.container}>
+      <FlatList
+        keyExtractor={item => item.id.toString()} 
+        data={contacts}
+        key={this}
+        initialNumToRender={Array.length}
+        renderItem={({item}) => {
+          return (
+          <TouchableOpacity>
+              <Text>{`${item.name} (${item.phoneNumbers ? item.phoneNumbers[0].number : ''})`}</Text>
+          </TouchableOpacity>
+          
+          )
+        }}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+
+  
+});
